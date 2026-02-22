7537fe9f84457f76dac193c3e0aec5c94b2ff6f1
ZaneSoCool <zanenicholaspederson@gmail.com>
2026-02-22 15:51:14 -0600

    more bubble work, not finished

--- Assets/Scripts/bubble.cs
+++ Assets/Scripts/bubble.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class bubble : MonoBehaviour
 {
+    Rigidbody2D rigidBody;
+
+    [SerializeField]
+    float bounceStrength = 50.0f;
+
+    [SerializeField]
+    Rigidbody2D playerRB;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rigidBody = gameObject.AddComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // called when the cube hits the floor
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            //bounce player
+            if (playerRB.linearVelocityX > playerRB.linearVelocityY){
+                playerRB.linearVelocityX = -playerRB.linearVelocityX * bounceStrength;
+            } else
+            {
+                playerRB.linearVelocityY = -playerRB.linearVelocityY * bounceStrength;
+            }
+            //playerRB.linearVelocity = new Vector2(-playerRB.linearVelocityX * bounceStrength, 0);
+        }
     }
 }
