5f724dd8f91fa7fa9be5c03d373cd322361d804e
ZaneSoCool <zanenicholaspederson@gmail.com>
2026-02-26 10:39:04 -0600

    player swim animation

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -3,15 +3,21 @@ using UnityEngine.InputSystem;
 
 public class player : MonoBehaviour
 {
+    
+    //Movement variables
     [SerializeField]
     float maxSpeed = 1.0f;
     [SerializeField]
     float acceleration = 0.1f;
     [SerializeField]
     float friction = 1.0f;
+
+    //Object references
     Rigidbody2D rigidBody;
     InputAction moveAction;
+    SpriteRenderer spriteRenderer;
 
+    //Health variables
     [SerializeField]
     int maxPlayerHealth = 5;
     public int playerHealth;
@@ -20,8 +26,12 @@ public class player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //find object references
         rigidBody = GetComponent<Rigidbody2D>();
         moveAction = InputSystem.actions.FindAction("Move");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        //setup player health
         playerHealth = maxPlayerHealth;
     }
 
@@ -31,6 +41,15 @@ public class player : MonoBehaviour
         if (moveAction.ReadValue<Vector2>() != new Vector2(0,0))
         {   //move player
             rigidBody.linearVelocity += acceleration * moveAction.ReadValue<Vector2>() * Time.deltaTime;
+
+            if (rigidBody.linearVelocity.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            } else if (rigidBody.linearVelocity.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+
         }  else
         {   //friction
             rigidBody.linearVelocity = Vector2.MoveTowards(rigidBody.linearVelocity, new Vector2(0,0), friction * Time.deltaTime);
