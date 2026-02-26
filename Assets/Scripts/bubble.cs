f2d63b8843922f299350dd8aadb4deff9aaf4b29
ZaneSoCool <zanenicholaspederson@gmail.com>
2026-02-26 10:02:48 -0600

    bubble complete

--- Assets/Scripts/bubble.cs
+++ Assets/Scripts/bubble.cs
@@ -5,7 +5,7 @@ public class bubble : MonoBehaviour
     Rigidbody2D rigidBody;
 
     [SerializeField]
-    float bounceStrength = 50.0f;
+    float bounceStrength = 3.0f;
 
     [SerializeField]
     Rigidbody2D playerRB;
@@ -23,18 +23,11 @@ public class bubble : MonoBehaviour
     }
 
     // called when the cube hits the floor
-    void OnCollisionEnter2D(Collision2D col)
+    void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
-            //bounce player
-            if (playerRB.linearVelocityX > playerRB.linearVelocityY){
-                playerRB.linearVelocityX = -playerRB.linearVelocityX * bounceStrength;
-            } else
-            {
-                playerRB.linearVelocityY = -playerRB.linearVelocityY * bounceStrength;
-            }
-            //playerRB.linearVelocity = new Vector2(-playerRB.linearVelocityX * bounceStrength, 0);
+            playerRB.linearVelocity *= -bounceStrength;
         }
     }
 }
