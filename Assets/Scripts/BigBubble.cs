850f46c26d1345d4f9ec2c717e02c64204cdff1f
wzy0403 <cwang2@macalester.edu>
2026-02-26 10:55:14 -0600

    Created BigBubble prefab

---
+++ Assets/Scripts/BigBubble.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class BigBubble : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+
+    [SerializeField]
+    float bounceStrength = 5.0f;
+
+    [SerializeField]
+    Rigidbody2D playerRB;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidBody = gameObject.AddComponent<Rigidbody2D>();
+    }
+    
+    // called when player bumps into bubble
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        playerRB = col.gameObject.GetComponent<Rigidbody2D>();
+        if (col.gameObject.CompareTag("Player"))
+        {
+            playerRB.linearVelocity *= -bounceStrength;
+        }
+    }
+}
