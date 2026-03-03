07d3c1397face1d53851fbfa53816b5511b35d16
ngumagay <124189045+ngumagay@users.noreply.github.com>
2026-03-03 11:08:35 -0600

    urchin script

--- Assets/Scripts/urchin.cs
+++ Assets/Scripts/urchin.cs
@@ -13,5 +13,33 @@ public class urchin : MonoBehaviour
     player gillgamesh;
 
 
+<<<<<<< Updated upstream
     // Start is called once before the fir
 }
+=======
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidBody = gameObject.AddComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerRB = collision.gameObject.GetComponent<Rigidbody2D>();
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerRB.linearVelocity *= -bounceStrength;
+            gillgamesh.playerHealth -= 1;
+            Debug.Log("Player Health = " + gillgamesh.playerHealth);
+        }
+
+    }
+}
+>>>>>>> Stashed changes
