b3f1d3b4e9b25b4c3c5bee6e5f28b2beb61d5cf6
ngumagay <124189045+ngumagay@users.noreply.github.com>
2026-04-11 13:31:56 -0500

    Merge branch 'main' of https://github.com/ZaneSoCool/Up-The-Fishissippi

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -20,6 +20,8 @@ public class player : MonoBehaviour
     SpriteRenderer spriteRenderer;
     Animator anim;
     BoxCollider2D tailThwapHitbox;
+    BoxCollider2D skullBashHitbox;
+    SkullBash skullBashScript;
     SpriteRenderer tailThwapSprite;
     
     //Health variables
@@ -32,8 +34,13 @@ public class player : MonoBehaviour
     public bool inputEnabled = true;
 
     //progression booleans
-    public bool hasTailThwap = false;
-    public bool hasSkullBash = false;
+    public bool hasTailThwap;
+    public bool hasSkullBash;
+
+    //vars for skullbash
+    private Vector2 skullBashDirection = new Vector2(1,0);
+    [SerializeField]
+    private float bashSpeed = 5.0f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -42,9 +49,13 @@ public class player : MonoBehaviour
         rigidBody = GetComponent<Rigidbody2D>();
         moveAction = InputSystem.actions.FindAction("Move");
         spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
         tailThwapHitbox = transform.Find("TailThwapHitbox").gameObject.GetComponent<BoxCollider2D>();
         tailThwapSprite = transform.Find("TailThwapHitbox").gameObject.GetComponent<SpriteRenderer>();
-        anim = GetComponent<Animator>();
+
+        skullBashHitbox = transform.Find("SkullBashHitbox").gameObject.GetComponent<BoxCollider2D>();
+        skullBashScript = transform.Find("SkullBashHitbox").GetComponent<SkullBash>();
 
         //setup player health
         playerHealth = maxPlayerHealth;
@@ -53,44 +64,52 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 direction = inputEnabled ? moveAction.ReadValue<Vector2>() : Vector2.zero;
+        if (skullBashScript.isSkullBashing == true) { //do dash 
+            skullBashDirection.Normalize(); //so speed is consistent even if player was barely inputting dir
+            rigidBody.linearVelocity = bashSpeed * skullBashDirection;
 
-        if (direction != new Vector2(0,0))
-        {   //move player
-            rigidBody.linearVelocity += acceleration * direction * Time.deltaTime;
+        } else { ///if not dashing move normally
+            
+            Vector2 direction = inputEnabled ? moveAction.ReadValue<Vector2>() : Vector2.zero;
 
-            if (rigidBody.linearVelocity.x > 0)
-            {
-                spriteRenderer.flipX = false;
-                tailThwapSprite.flipX = false;
+            if (direction != new Vector2(0,0)) //if inputing direction move player
+            {   
+                //set skullBashDirection
+                skullBashDirection = direction;
 
-            } else if (rigidBody.linearVelocity.x < 0)
-            {
-                spriteRenderer.flipX = true;
-                tailThwapSprite.flipX = true;
+                rigidBody.linearVelocity += acceleration * direction * Time.deltaTime; //move player
+
+            }  else { //if not inputing direction -> friction
+                rigidBody.linearVelocity = Vector2.MoveTowards(rigidBody.linearVelocity, new Vector2(0,0), friction * Time.deltaTime);
             }
 
-        }  else
-        {   //friction
-            rigidBody.linearVelocity = Vector2.MoveTowards(rigidBody.linearVelocity, new Vector2(0,0), friction * Time.deltaTime);
+            rigidBody.linearVelocity = Vector2.ClampMagnitude(rigidBody.linearVelocity, maxSpeed); //clamp max speed
+        }
+        
+        //flip sprite if needed
+        if (rigidBody.linearVelocity.x > 0){
+            spriteRenderer.flipX = false;
+            tailThwapSprite.flipX = false;
+
+        } else if (rigidBody.linearVelocity.x < 0){
+            spriteRenderer.flipX = true;
+            tailThwapSprite.flipX = true;
         }
 
-        //clamp max speed
-        rigidBody.linearVelocity = Vector2.ClampMagnitude(rigidBody.linearVelocity, maxSpeed);
-
-
-        //rotate player based on vertical speed
+        //rotate player & hitboxes based on vertical speed
         if (rigidBody.linearVelocity != new Vector2(0,0)){
             float angle = Mathf.Atan2(rigidBody.linearVelocity.y, rigidBody.linearVelocity.x) * Mathf.Rad2Deg;
             if (spriteRenderer.flipX == false)
             {
                 gameObject.transform.rotation = Quaternion.Euler(0f, 0f, angle);
                 tailThwapHitbox.transform.localPosition = new Vector2(1f, 0f);
+                skullBashHitbox.transform.localPosition = new Vector2(0.5f, 0f);
 
             } else{
                 angle -= 180;
                 gameObject.transform.rotation = Quaternion.Euler(0f, 0f, angle);
                 tailThwapHitbox.transform.localPosition = new Vector2(-1f, 0f);
+                skullBashHitbox.transform.localPosition = new Vector2(-0.5f, 0f);
             }
         }
 
@@ -112,11 +131,5 @@ public class player : MonoBehaviour
     public void resetAnimation() //called when abnormal player animation finishes
     {
         isDoingSpecialAnim = false;
-    }
-
-    public void OnBashAnimEnded()
-    {
-        SkullBash skullBashScript = transform.Find("SkullBashHitbox").GetComponent<SkullBash>();
-        skullBashScript.isSkullBashing = false;
     }
 }
