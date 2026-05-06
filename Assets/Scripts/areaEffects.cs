f479aa2e3f9ff4ce21d4a5dd67cccb3a15c6c365
ZaneSoCool <zanenicholaspederson@gmail.com>
2026-05-06 12:37:48 -0500

    more geyser goop
    -fixed layering
    -fixed colors
    -changed colors of area 2

--- Assets/Scripts/areaEffects.cs
+++ Assets/Scripts/areaEffects.cs
@@ -12,9 +12,17 @@ public class areaEffects : MonoBehaviour
     [SerializeField]
     private GameObject hiddenWallTiles; 
     [SerializeField]
+    private GameObject backgroundWallTiles; 
+    [SerializeField]
+    private GameObject backgroundWallTiles2; 
+    [SerializeField]
     private Color area2backColor;
     [SerializeField]
     private Color area2wallColor;
+    [SerializeField]
+    private Color area2backWallColor;
+    [SerializeField]
+    private Color area2backWall2Color;
 
     void Start()
     {
@@ -29,5 +37,11 @@ public class areaEffects : MonoBehaviour
 
         Tilemap hiddenTilemap = hiddenWallTiles.GetComponent<Tilemap>();
         hiddenTilemap.color = area2wallColor;
+
+        Tilemap backgroundTilemap = backgroundWallTiles.GetComponent<Tilemap>();
+        backgroundTilemap.color = area2backWallColor;
+
+        Tilemap backgroundTilemap2 = backgroundWallTiles2.GetComponent<Tilemap>();
+        backgroundTilemap2.color = area2backWall2Color;
     }
 }
